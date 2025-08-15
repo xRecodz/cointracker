b4c40c0cb3df14203699ef114e0caa7625d13f1c
xrecodz <farhannix9@gmail.com>
2025-08-15 21:08:16 +0700

    Update

--- non-used/utils/helpers.js
+++
@@ -1,8 +0,0 @@
-export const CHAINS = {
-  ethereum: { cgId: "ethereum", symbol: "ETH", chainIdHex: "0x1" },
-  bsc: { cgId: "binancecoin", symbol: "BNB", chainIdHex: "0x38" },
-  polygon: { cgId: "matic-network", symbol: "MATIC", chainIdHex: "0x89" },
-};
-
-export const fmtUSD = (n) => n?.toLocaleString(undefined, { style: 'currency', currency: 'USD', maximumFractionDigits: 2 });
-export const cls = (...a) => a.filter(Boolean).join(' ');
